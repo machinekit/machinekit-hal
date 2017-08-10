feb4f1d0c54711061dcd11686ee43685ab407573
Mick <arceye@mgware.co.uk>
2017-08-10 16:55:47 +0100

    Merge branch 'update-build-debs' into complete-builder-changes

--- .travis/save_ccache.sh
+++ .travis/save_ccache.sh
@@ -6,14 +6,14 @@ if [ ! -z ${FLAV+x} ]; then
 fi
 
 # only upload if ssh key is present
-if [ -f ~/.ssh/id_rsa ]; then
-    cd ${TRAVIS_BUILD_DIR}/../ccache
-    git init
-    git add -A
-    git config --global user.email "travis-ci@machinekit.io"
-    git config --global user.name "travis-ci"
-    git commit -m ccache 2>&1 >/dev/null
-    git remote add origin git@github.com:${CCACHE_REPO}.git
-    git checkout -b ${branch}
-    git push --set-upstream origin ${branch} --force || true
-fi
+#if [ -f ~/.ssh/id_rsa ]; then
+#    cd ${TRAVIS_BUILD_DIR}/../ccache
+#    git init
+#    git add -A
+#    git config --global user.email "travis-ci@machinekit.io"
+#    git config --global user.name "travis-ci"
+#    git commit -m ccache 2>&1 >/dev/null
+#    git remote add origin git@github.com:${CCACHE_REPO}.git
+#    git checkout -b ${branch}
+#    git push --set-upstream origin ${branch} --force || true
+#fi
--- .travis/upload_packagecloud.sh
+++ .travis/upload_packagecloud.sh
@@ -25,7 +25,6 @@ if [ "${TRAVIS_TEST_RESULT}" -eq 0 ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ] \
     # have already been uploaded
     if [ "${FLAV}" == "rt_preempt" ] || [ "${FLAV}" == "xenomai" ]; then
         rm -f ${TRAVIS_BUILD_DIR}/deploy/machinekit_*
-        rm -f ${TRAVIS_BUILD_DIR}/deploy/machinekit-dev*
     fi
 
     package_cloud push ${repo} ${TRAVIS_BUILD_DIR}/deploy/*deb
