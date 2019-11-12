8b15af79768398cfe9572e77d83bf360a6e3bee8
GP Orcullo <kinsamanka@gmail.com>
2019-11-12 23:35:04 +0000

    FindPython2_local.cmake: added missing variable

--- cmake/FindPython2_local.cmake
+++ cmake/FindPython2_local.cmake
@@ -8,6 +8,7 @@
 # Python2_SITELIB
 # Python2_EXECUTABLE
 # Python2_VERSION
+# Python2_INCLUDE_DIRS
 
 find_package(PythonInterp ${Python2_local_FIND_VERSION} REQUIRED)
 find_package(PythonLibs ${Python2_local_FIND_VERSION} REQUIRED)
@@ -24,6 +25,9 @@ set (Python2_EXECUTABLE ${PYTHON_EXECUTABLE})
 set (Python2_VERSION ${PYTHON_VERSION_STRING})
 
 execute_process (
-  COMMAND python -c "from distutils.sysconfig import get_python_lib; print get_python_lib()" 
-  OUTPUT_VARIABLE Python2_SITELIB  OUTPUT_STRIP_TRAILING_WHITESPACE)	
+  COMMAND python -c "from distutils.sysconfig import get_python_lib; print get_python_lib()"
+  OUTPUT_VARIABLE Python2_SITELIB  OUTPUT_STRIP_TRAILING_WHITESPACE)
 
+execute_process (
+  COMMAND python -c "from distutils.sysconfig import get_python_inc; print get_python_inc()"
+  OUTPUT_VARIABLE Python2_INCLUDE_DIRS  OUTPUT_STRIP_TRAILING_WHITESPACE)
