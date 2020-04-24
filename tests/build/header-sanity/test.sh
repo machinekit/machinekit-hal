0e9e06abd49bb6b5a3c53bdb2e103a1e0d4abe10
Jakub Fišer <jakub.fiser@eryaf.com>
2020-04-24 23:48:07 +0200

    Remove old non-existent headers (anymore) from test

--- tests/build/header-sanity/test.sh
+++ tests/build/header-sanity/test.sh
@@ -5,8 +5,6 @@ for i in $HEADERS/*.h; do
     case $i in
     */rtapi_app.h) continue ;;
     */rtapi_common.h) continue ;;
-    */rtai-kernel.h) continue ;;
-    */xenomai-kernel.h) continue ;;
     */*.pb.h) continue ;;
     */container.h) continue ;;
     esac
@@ -16,9 +14,6 @@ for i in $HEADERS/*.h $HEADERS/*.hh; do
     case $i in
     */rtapi_app.h) continue ;;
     */rtapi_common.h) continue ;;
-    */interp_internal.hh) continue ;;
-    */rtai-kernel.h) continue ;;
-    */xenomai-kernel.h) continue ;;
     */*.pb.h) continue ;;
     */container.h) continue ;;
     esac
