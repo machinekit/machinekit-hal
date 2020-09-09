3d9db9f5fce6ef709168a0c076097ded41fb46e3
John Morris <john@zultron.com>
2020-09-09 14:04:53 -0500

    cython:  Fix `compat` test str/bytes problems
    - `is_module_loaded()` method no longer exists.
    - Fix str/bytes problems in `compat.get_rtapi_config()`

--- nosetests/test_compat.py
+++ nosetests/test_compat.py
@@ -7,9 +7,10 @@ from machinekit import compat
 @pytest.mark.usefixtures('realtime')
 class TestCompat(object):
 
-    def test_compat(self):
-        # nonexistant kernel module
-        assert compat.is_module_loaded("foobarbaz") == False
+    def test_get_rtapi_config(self):
+        ld = compat.get_rtapi_config('LIBEXEC_DIR')
+        print(repr(ld))
+        assert ld.endswith("libexec")
 
 (lambda s=__import__('signal'):
      s.signal(s.SIGTERM, s.SIG_IGN))()
--- src/hal/cython/machinekit/compat.pyx
+++ src/hal/cython/machinekit/compat.pyx
@@ -5,7 +5,8 @@ from os import strerror
 
 def get_rtapi_config(param):
     cdef char result[1024]
-    rc = c_get_rtapi_config(result, param, 1024)
+    param_b = param.encode()
+    rc = c_get_rtapi_config(result, param_b, 1024)
     if rc:
         raise RuntimeError(f"get_rtapiconfig({param}) failed: {rc} {strerror(-rc)}")
-    return str(<char *>result)
+    return result.decode()
