efbbc5cad0f24ea6247d2e0cf7c0d597f9fec4c0
Jakub Fišer <jakub.fiser@eryaf.com>
2021-07-26 12:59:02 +0200

    MAINTENANCE: Turn off LTO and implement SETUID target
    Ubuntu 21.04 Hirsute started to return '-flto=auto -ffat-lto-objects' via 'dpkg-buildflags' in CFLAGS and CXXFLAGS and '-flto=auto' in LDFLAGS, thus activating the Link-Time Optimization. That started to clash with linker script which is hiding the local symbols in HAL managed modules. As a temporary solution, this commit removes aforementioned flags from build.
    
    New target 'setuid' was added to allow setting of setuid bit for 'rtapi_App', 'pci_write' and 'pci_read' when run from BUILD tree. Call with 'sudo make setuid' (when using Makefiles). Also override the dh_fixperms to do the same thing when building packages.

--- src/libraries/symbol_visibility/src/MachinekitHALSymbolVisibility.cmake
+++ src/libraries/symbol_visibility/src/MachinekitHALSymbolVisibility.cmake
@@ -31,13 +31,13 @@ set(RTAPI_STRIPPER_SCRIPT
     [=[
 #!/bin/env bash
 
-readarray -td\\\\; a <<<"$<TARGET_OBJECTS:@PERTINENT_TARGET@>"
+readarray -td\; a <<<"$<TARGET_OBJECTS:@rtapi_export_TARGET@>"
 
 GLOBAL_SYMBOLS=""
 
 for i in ${a[@]}
 do
-    SYMS="$(@OBJ_COPY@ -j .rtapi_export -O binary ${i} /dev/stdout | tr -s '\0' | xargs -r0 printf '%s\\\;\n' | grep .)"
+    SYMS="$(@CMAKE_OBJCOPY@ -j .rtapi_export -O binary ${i} /dev/stdout | tr -s '\0' | xargs -r0 printf '%s;\n' | grep .)"
     GLOBAL_SYMBOLS+="$SYMS"
 done
 
@@ -52,10 +52,10 @@ fi
 
 MAP=$(printf "%b\n" \
     "{ $GLOBAL_MAP" \
-    "local : * \\\;" \
-    "}\\\;")
+    "local : * ;" \
+    "};")
 
-echo "$MAP" >@OUTPUT_FILE@
+echo "$MAP" >@linker_file_map@
 ]=])
 
 function(export_rtapi_symbols)
@@ -66,24 +66,19 @@ function(export_rtapi_symbols)
   cmake_parse_arguments(${prefix} "${noValues}" "${singleValues}"
                         "${multiValues}" ${ARGN})
 
-  set(linkerFileDirectory
+  set(linker_file_directory
       "${CMAKE_CURRENT_BINARY_DIR}/$<CONFIG>/linker/${${prefix}_TARGET}")
-  set(linkerFileMap "${linkerFileDirectory}/rtapi_export_symbols.map")
-  set(linkerFileScript "${linkerFileDirectory}/rtapi_stripper.sh")
+  set(linker_file_map "${linker_file_directory}/rtapi_export_symbols.t")
+  set(linker_file_script "${linker_file_directory}/rtapi_stripper.sh")
 
-  string(REPLACE "@PERTINENT_TARGET@" "${${prefix}_TARGET}"
-                 target_stripper_script ${RTAPI_STRIPPER_SCRIPT})
-  string(REPLACE "@OUTPUT_FILE@" "${linkerFileMap}" target_stripper_script
-                 ${target_stripper_script})
-  string(REPLACE "@OBJ_COPY@" "${CMAKE_OBJCOPY}" target_stripper_script
-                 ${target_stripper_script})
+  string(CONFIGURE "${RTAPI_STRIPPER_SCRIPT}" target_stripper_script @ONLY)
 
-  file(REMOVE_RECURSE "${linkerFileDirectory}")
+  file(REMOVE_RECURSE "${linker_file_directory}")
 
   # File GENERATE will create the needed directory structure
   file(
     GENERATE
-    OUTPUT "${linkerFileScript}"
+    OUTPUT "${linker_file_script}"
     CONTENT "${target_stripper_script}"
             TARGET
             ${${prefix}_TARGET}
@@ -100,11 +95,11 @@ function(export_rtapi_symbols)
   add_custom_command(
     TARGET ${${prefix}_TARGET}
     PRE_LINK
-    COMMAND ${CMAKE_COMMAND} "-E" "env" "${BASH}" "${linkerFileScript}"
-            BYPRODUCT "${linkerFileMap}"
+    COMMAND ${CMAKE_COMMAND} "-E" "env" "${BASH}" "${linker_file_script}"
+            BYPRODUCT "${linker_file_map}"
     VERBATIM)
 
   target_link_options(${${prefix}_TARGET} PRIVATE
-                      "LINKER:--version-script=${linkerFileMap}")
+                      "LINKER:--version-script=${linker_file_map}")
 
 endfunction()
